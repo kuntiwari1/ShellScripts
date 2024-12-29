05af1d5c3327875e2cc3b48a05282094ae2eaaba
Kunal Tiwari <kunaltiwari49087@gmail.com>
2024-12-29 16:15:17 +0530

    Save work before pulling with rebase

--- 05_arrays.sh
+++ 05_arrays.sh
@@ -9,3 +9,9 @@ echo "Values in 3rd index ${myArra[3]}"
 
 #How to find no. of values in an array
 echo "No. of values, length of an array is ${myArray[*]}"
+
+#Updating our array with new values
+myArray+=( New 30 40)
+
+echo "Values of new array are ${myArray[*]}"
+
