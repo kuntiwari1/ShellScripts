38648d2fa413466391955149f7e26443df46fc25
Kunal Tiwari <kunaltiwari49087@gmail.com>
2024-12-29 15:56:20 +0530

    Updated 05_arrays.sh

--- 05_arrays.sh
+++ 05_arrays.sh
@@ -6,3 +6,6 @@ myArray=( 1 20 30.5 Hello "Hey Boy!")
 echo "All the values in array are ${myArray[*]}"
 
 echo "Values in 3rd index ${myArra[3]}"
+
+#How to find no. of values in an array
+echo "No. of values, length of an array is ${myArray[*]}"
